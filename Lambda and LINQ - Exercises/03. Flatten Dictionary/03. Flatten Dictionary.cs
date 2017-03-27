f35e04c54df004ac3e3f7b05fb6702dfbfd30e07
Warglaive <warglaivegm@gmail.com>
2017-03-27 19:11:12 +0300

    03. Flatten Dictionary Update

--- Lambda and LINQ - Exercises/03. Flatten Dictionary/03. Flatten Dictionary.cs
+++ Lambda and LINQ - Exercises/03. Flatten Dictionary/03. Flatten Dictionary.cs
@@ -1,20 +1,60 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 namespace _03.Flatten_Dictionary
 {
     public class Program
     {
         public static void Main()
         {
-            var input = Console.ReadLine();
-            var store = new Dictionary<List<string>, Dictionary<string, string>>();
-            while (input!="end")
+            var inputLine = Console.ReadLine();
+            var dictionary = new Dictionary<string, Dictionary<string, string>>();
+            while (inputLine!="end")
             {
-                
-                input = Console.ReadLine();
+                var inputParams = inputLine.Split(' ').ToList();
+                if (inputParams[0] != "flatten")
+                {
+                    var key = inputParams[0];
+                    var innerKey = inputParams[1];
+                    var innerValue = inputParams[2];
+                    if (!dictionary.ContainsKey(key))
+                    {
+                        dictionary.Add(key, new Dictionary<string, string>());
+                    }
+                        dictionary[key][innerKey] = innerValue;
+                }
+                else
+                {
+                    var key = inputParams[1];
+                    dictionary[key] = dictionary[key].ToDictionary(x => x.Key + x.Value, x => "flattened");
+                }
+                inputLine = Console.ReadLine();
+            }
+            var orderedDictionary = new Dictionary<string, Dictionary<string, string>>()
+                .OrderByDescending(x => x.Key.Length)
+                .ToDictionary(x => x.Key, x => x.Value);
+            foreach (var entry in dictionary.OrderByDescending(x => x.Key.Length)) 
+            {
+                Console.WriteLine("{0}", entry.Key);
+                Dictionary<string, string> orderedInnerDictionary = entry.Value
+                    .Where(x => x.Value != "flattened")
+                    .OrderBy(x => x.Key.Length)
+                    .ToDictionary(x => x.Key, x => x.Value);
+                Dictionary<string, string> flattenedValues =
+                    entry.Value
+                    .Where(x => x.Value == "flattened")
+                    .ToDictionary(x => x.Key, x => x.Value);
+                int count = 0;
+                foreach (var innerEntry in orderedInnerDictionary)
+                {
+                    count++;
+                    Console.WriteLine("{0}. {1} - {2}", count, innerEntry.Key, innerEntry.Value);
+                }
+                foreach (var flattened in flattenedValues)
+                {
+                    count++;
+                    Console.WriteLine("{0}. {1}",count, flattened.Key);
+                }
             }
         }
     }
